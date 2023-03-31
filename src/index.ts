7e1f66c5cdbedbcfae31fb418c35016e2084d9a6
Emilio P <ea.pugaavalos.99@gmail.com>
2023-03-31 17:41:33 -0600

    Agregar ruta para visualizar imagen, para evitar el hotlinking, y agregar ruta para ver la información de un solo post a partir de su id

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,40 @@ const axios_gelbooru = axios.create({
   baseURL: base_gelbooru_url
 })
 
+/*
+  URLS THAT DON'T REQUIRE HEADERS
+*/
+
 // Dummy url
 app.post('/login', (req, res) => {
   res.send('Logged in!')
 })
+
+// Get image
+app.get('/image', async (req, res) => {
+  try {
+    if (!req.query.url) {
+      res.status(StatusCodes.BAD_REQUEST).send('Please provide a url')
+      return
+    }
+
+    const resGelImg = await axios.get(`${req.query.url}`, { responseType: 'arraybuffer' })
+
+    console.log(resGelImg)
+
+    res.setHeader('content-type', resGelImg.headers['content-type']?.toString() ?? 'image/jpeg')
+    res.status(StatusCodes.OK).send(resGelImg.data)
+
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+  }
+})
+
+/*
+  URLS THAT REQUIRE HEADERS
+*/
+
 
 app.use((req, res, next) => {
   if (req.headers.api_key && req.headers.user_id) {
@@ -38,6 +68,24 @@ app.use((req, res, next) => {
 })
 
 const videoTags = ['animated', 'video']
+function convertPost(post: GelbooruPost) {
+  post.created_at_date = new Date(post.created_at)
+
+  post.tags_array = he.decode(post.tags).split(' ')
+  post.source_array = post.source.split('|').map((src) => src.trim())
+
+  post.has_note_bool = post.has_notes === 'true'
+  post.has_comments_bool = post.has_comments === 'true'
+  post.has_children_bool = post.has_children === 'true'
+
+  post.is_video = post.tags_array.some((tag) => videoTags.includes(tag))
+  post.is_3d = post.tags_array.includes('3d')
+  post.is_irl = post.tags_array.includes('photo_(medium)')
+  post.is_sound = post.tags_array.includes('sound')
+
+  return post
+}
+
 app.get('/post', async (req, res) => {
   try {
     const resGel = await axios_gelbooru.get('', {
@@ -60,24 +108,42 @@ app.get('/post', async (req, res) => {
     }
 
     resGel.data.post.forEach((post: GelbooruPost) => {
-      post.created_at_date = new Date(post.created_at)
-
-      post.tags_array = he.decode(post.tags).split(' ')
-      post.source_array = post.source.split('|').map((src) => src.trim())
-
-      post.has_note_bool = post.has_notes === 'true'
-      post.has_comments_bool = post.has_comments === 'true'
-      post.has_children_bool = post.has_children === 'true'
-
-      post.is_video = post.tags_array.some((tag) => videoTags.includes(tag))
-      post.is_3d = post.tags_array.includes('3d')
-      post.is_irl = post.tags_array.includes('photo_(medium)')
-      post.is_sound = post.tags_array.includes('sound')
+      post = convertPost(post)
     })
 
     res.send(resGel.data)
   } catch (error) {
     console.error(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR)
+  }
+})
+
+app.get('/post/:id', async (req, res) => {
+  try {
+    const resGel = await axios_gelbooru.get('', {
+      params: {
+        page: 'dapi',
+        q: 'index',
+        s: 'post',
+        json: 1,
+        id: req.params.id,
+        api_key: req.headers.api_key,
+        user_id: req.headers.user_id,
+      }
+    })
+
+    if (!('post' in resGel.data)) {
+      res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND)
+      return
+    }
+
+    resGel.data.post.forEach((post: GelbooruPost) => {
+      post = convertPost(post)
+    })
+
+    res.send(resGel.data)
+  } catch (error) {
+    console.log(error)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ReasonPhrases.INTERNAL_SERVER_ERROR)
   }
 })
